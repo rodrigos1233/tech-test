e54130820d164d0377cfcf0b54b9febed844233a
Rodrigo <rodrigopsalazar@hotmail.com>
2021-03-10 17:36:32 +0100

    change rating on click

--- frontend/src/components/Rating/index.jsx
+++ frontend/src/components/Rating/index.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Star from '../../assets/icons/star.svg'
 import YellowStar from '../../assets/icons/yellow-star.svg'
 import './Rating.scss'
 
 function Rating({rating}) {
 
+    const [updatedRating, setUpsatedRating] = useState(rating)
+
+    function handleClick(k) {
+        setUpsatedRating(k)
+    }
+
     return (
         <div className="rating-stars">
             {Array.from({ length: 5 }, (_, k) => (
                 <img
                     key={k}
-                    src={k >= rating ? Star : YellowStar}
+                    src={k >= updatedRating ? Star : YellowStar}
+                    onClick={() => handleClick(k + 1)}
                 />
             ))}
 
