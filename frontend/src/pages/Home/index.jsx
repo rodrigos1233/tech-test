d1399eeb6a481075bec956668d53e99bb90f81e1
Rodrigo <rodrigopsalazar@hotmail.com>
2021-03-11 16:26:16 +0100

    delete static bookstores

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,53 +2,6 @@ import React, { useState, useEffect } from 'react'
 import BookStore from './../../components/BookStore'
 import './Home.scss'
 
-const bookStores = [
-    {
-        id: 1,
-        attributes: {
-            name: 'book store 1',
-            storeImage: 'https://i.pinimg.com/736x/51/a2/47/51a247e0d1785b89b70a17a1c8f31ac5--melbourne-australia-second-hand.jpg',
-            establishmentDate: '1995-02-09T00:00:00+0000',
-            website:'https://www.micro-books-store.com',
-            rating: 4,
-            country: 'CH',
-            books: [
-                {
-                    copiesSold: 47,
-                    author: 'Ivelin Demirov',
-                    id: 4,
-                    name: 'Canvas Pocket Reference: Scripted Graphics for HTML5',
-                },
-                {
-                    copiesSold: 27,
-                    author: 'Ivelin Demirov',
-                    id: 5,
-                    name: 'Canvas Pocket Reference: HTML5',
-                },
-                {
-                    copiesSold: 147,
-                    author: 'Ivelin Demirov',
-                    id: 6,
-                    name: 'Canvas: Graphics',
-                },
-
-            ]
-        }
-    },
-    {
-        id: 2,
-        attributes: {
-            name: 'book store 2',
-            storeImage: 'https://i.pinimg.com/736x/51/a2/47/51a247e0d1785b89b70a17a1c8f31ac5--melbourne-australia-second-hand.jpg',
-            establishmentDate: '1994-02-09T00:00:00+0000',
-            website:'https://www.micro-books-store.com',
-            rating: 2,
-            country: 'SE',
-            books: []
-        }
-    },
-]
-
 function Home() {
     const fetchStoresDataUrl = 'http://localhost:3000/stores'
     const fetchBooksDataUrl = 'http://localhost:3000/books'
@@ -130,17 +83,7 @@ function Home() {
         const countryId = store.relationships.countries.data.id
         const country = jsonCountries.filter(jsonCountry => jsonCountry.id === countryId)
         store.attributes.country = country[0]?.attributes?.code
-
-        console.log(store)
-
-        
     })
-
-
-
-
-
-
 
     return (
         <div className="bookstores">
